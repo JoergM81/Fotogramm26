315f3bab9b449af54106a5fe8bdfafdd1dc7efd5
Joerg Marchner <joerg@marchner.de>
2026-02-14 19:04:50 +0100

    Dialog buttons

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const dialog = document.getElementById("imageDialog");
 const dialogImage = document.getElementById("dialogImage");
 const dialogTitle = document.getElementById("dialogTitle");
 
-window.openDialog = function(imgSrc, imgName){
+window.openDialog = function(i, imgSrc, imgName){
+    index = i;
     dialogImage.src = imgSrc;
     dialogTitle.textContent = imgName;
     dialog.showModal();
